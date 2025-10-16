7b9277f28b1c8fcbba2559f01c2ba710821e2cf2
Tu Nombre <sergioj.casanash@gmail.com>
2025-10-16 12:00:51 -0400

    feat: actualizar el menú de usuario y mejorar la estructura del encabezado

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -11,14 +11,12 @@
         <div class="m-header">
             <a href="../dashboard/index.html" class="b-brand text-center">
                 <!-- ========   Change your logo from here   ============ -->
-                <img src="{{asset('img/header/diapo.svg')}}" alt="logo image" class="logo-lg ms-4" width="auto" height="35" />
+                <img src="{{asset('img/header/diapo.svg')}}" alt="logo image" class="logo-lg ms-4" width="auto"
+                    height="35" />
             </a>
         </div>
         <div class="navbar-content">
             <ul class="pc-navbar">
-                <li class="pc-item pc-caption">
-                    <label>Navigation</label>
-                </li>
                 <li class="pc-item">
                     <a href="../dashboard/index.html" class="pc-link">
                         <span class="pc-micon">
@@ -26,29 +24,6 @@
                         </span>
                         <span class="pc-mtext">Dashboard</span>
                     </a>
-                </li>
-
-                <li class="pc-item pc-caption">
-                    <label>Pages</label>
-                    <i class="material-icons-two-tone">desktop_windows</i>
-                    <span>15+ Redymade Pages</span>
-                </li>
-                <li class="pc-item">
-                    <a href="../pages/login-v1.html" class="pc-link" target="_blank">
-                        <span class="pc-micon"><i class="material-icons-two-tone">verified_user</i></span>
-                        <span class="pc-mtext">Login</span>
-                    </a>
-                </li>
-                <li class="pc-item">
-                    <a href="../pages/register-v1.html" class="pc-link" target="_blank">
-                        <span class="pc-micon"><i class="material-icons-two-tone">person_add_alt_1</i></span>
-                        <span class="pc-mtext">Register</span>
-                    </a>
-                </li>
-                <li class="pc-item pc-caption">
-                    <label>Other</label>
-                    <i class="material-icons-two-tone">list_alt</i>
-                    <span>Extra More Things</span>
                 </li>
                 <li class="pc-item pc-hasmenu">
                     <a href="#!" class="pc-link">
@@ -101,22 +76,12 @@
                     </ul>
                 </li>
                 <li class="pc-item">
-                    <a href="../other/sample-page.html" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="material-icons-two-tone">storefront</i>
-                        </span>
-                        <span class="pc-mtext">Sample page</span>
+                    <a href="../pages/register-v1.html" class="pc-link" target="_blank">
+                        <span class="pc-micon"><i class="material-icons-two-tone">person_add_alt_1</i></span>
+                        <span class="pc-mtext">Register</span>
                     </a>
                 </li>
             </ul>
-            <div class="container">
-                <h1>Dashboard</h1>
-                <p>Welcome, {{ Auth::user()->name }}!</p>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit">Logout</button>
-                </form>
-            </div>
         </div>
     </div>
 </nav>
