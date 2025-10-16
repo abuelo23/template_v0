f5ab63f10fd76eaa49cc5d80d5a507d92e0e1ce0
sergioj.casanash <sergioj.casanash@gmail.com>
2025-10-16 15:19:31 +0000

    feat: cambiar de sitio el Logout

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,12 +1,5 @@
 @extends('layout.index')
 
 @section('content')
-    <div class="container">
-        <h1>Dashboard</h1>
-        <p>Welcome, {{ Auth::user()->name }}!</p>
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit">Logout</button>
-        </form>
-    </div>
+    
 @endsection
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -109,6 +109,14 @@
                     </a>
                 </li>
             </ul>
+            <div class="container">
+                <h1>Dashboard</h1>
+                <p>Welcome, {{ Auth::user()->name }}!</p>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            </div>
         </div>
     </div>
 </nav>
