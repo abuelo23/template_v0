2cfca45e14b3518a5cb78fd1672d099795378f66
Tu Nombre <sergioj.casanash@gmail.com>
2025-10-17 11:23:48 -0400

    feat: limpieza

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -9,18 +9,15 @@
 <nav class="pc-sidebar">
     <div class="navbar-wrapper">
         <div class="m-header">
-            <a href="../dashboard/index.html" class="b-brand text-center">
+            <a href="#!" class="b-brand text-center">
                 <!-- ========   Change your logo from here   ============ -->
                 <img src="{{asset('img/header/diapo.svg')}}" alt="logo image" class="logo-lg ms-4" width="auto" height="35" />
             </a>
         </div>
         <div class="navbar-content">
             <ul class="pc-navbar">
-                <li class="pc-item pc-caption">
-                    <label>Navigation</label>
-                </li>
                 <li class="pc-item">
-                    <a href="../dashboard/index.html" class="pc-link">
+                    <a href="#!" class="pc-link">
                         <span class="pc-micon">
                             <i class="material-icons-two-tone">home</i>
                         </span>
@@ -28,27 +25,11 @@
                     </a>
                 </li>
 
-                <li class="pc-item pc-caption">
-                    <label>Pages</label>
-                    <i class="material-icons-two-tone">desktop_windows</i>
-                    <span>15+ Redymade Pages</span>
-                </li>
                 <li class="pc-item">
-                    <a href="../pages/login-v1.html" class="pc-link" target="_blank">
-                        <span class="pc-micon"><i class="material-icons-two-tone">verified_user</i></span>
-                        <span class="pc-mtext">Login</span>
-                    </a>
-                </li>
-                <li class="pc-item">
-                    <a href="../pages/register-v1.html" class="pc-link" target="_blank">
+                    <a href="#!" class="pc-link" target="_blank">
                         <span class="pc-micon"><i class="material-icons-two-tone">person_add_alt_1</i></span>
                         <span class="pc-mtext">Register</span>
                     </a>
-                </li>
-                <li class="pc-item pc-caption">
-                    <label>Other</label>
-                    <i class="material-icons-two-tone">list_alt</i>
-                    <span>Extra More Things</span>
                 </li>
                 <li class="pc-item pc-hasmenu">
                     <a href="#!" class="pc-link">
@@ -100,14 +81,7 @@
                         </li>
                     </ul>
                 </li>
-                <li class="pc-item">
-                    <a href="../other/sample-page.html" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="material-icons-two-tone">storefront</i>
-                        </span>
-                        <span class="pc-mtext">Sample page</span>
-                    </a>
-                </li>
+                
             </ul>
         </div>
     </div>
